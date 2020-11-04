5707839623eb0e8a1ad5c5c7e7b26e71321b37f3
Warzecha <damian.warzecha1@gmail.com>
2020-11-04 22:43:30 +0100

    added burning the midnight oil achievement

--- src/services/integrations/GithubWebhookHandler.js
+++ src/services/integrations/GithubWebhookHandler.js
@@ -1,5 +1,6 @@
 const AchievementProgressService = require('../AchievementProgressService');
 const UsersService = require('../UserService');
+const moment = require('moment');
 
 
 const handleEvent = async (event, payload) => {
@@ -36,6 +37,13 @@ const handlePushEvent = async (payload) => {
 
     const commitCount = commits.length;
 
+
+    let isAtNight = commits
+        .map(({timestamp}) => moment(timestamp))
+        .map((commitTime) => commitTime.isBetween(commitTime.hour(0).minute(0).second(0),
+            commitTime.hour(5).minute(0).second(0)))
+        .find(value => value === true);
+
     const matchingUsers = await UsersService.getAllByGithubUsername(name);
 
     matchingUsers.forEach(u => {
@@ -57,7 +65,21 @@ const handlePushEvent = async (payload) => {
                     console.error(err);
                 });
         });
+
+        if (isAtNight) {
+            AchievementProgressService.updateProgress(user.id, 'burning-the-midnight-oil', {increaseScore: 1})
+                .then(res => {
+                    console.log('Updated achievement progress', res);
+                })
+                .catch(err => {
+                    console.error('Failed to update achievement progress');
+                    console.error(err);
+                });
+        }
+
     });
+
+
 };
 
 module.exports = {handleEvent};
